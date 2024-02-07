2e13d2ce0f9d13824af0ced9d9917d195788b4b7
Katie Sharples <KSharples431@gmail.com>
2024-02-07 12:29:31 -0500

    Fixed birthday format

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -37,13 +37,14 @@ const signupUser = async (req, res, next) => {
   try {
     const user = await User.signup(username, email, password, birthday);
     const token = createToken(user._id);
+    const displayBirthday = new Date(user.birthday).toISOString().split('T')[0];
     const userInfo = {
       username: user.username,
       email: user.email,
       token: token,
       id: user._id,
       image: user.imagePath,
-      birthday: user.birthday,
+      birthday: displayBirthday,
       favorites: user.favorites,
     };
 
