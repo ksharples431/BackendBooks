dd83767c794abc2c18448b2d6e8982492c11d1ae
Katie Sharples <KSharples431@gmail.com>
2024-06-14 15:26:22 -0400

    Changed sameSite in generateToken

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -8,7 +8,7 @@ const createToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    // sameSite: 'strict',
     maxAge: 30 * 24 * 60 * 60 * 1000
   });
 };
